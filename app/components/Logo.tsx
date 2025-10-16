db6a7983ff120b3277c9aece8b479c3d44588333
Vlad Smolianets <itworks.by.vlad@gmail.com>
2025-10-16 20:32:31 +0300

    In progress

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,8 +2,8 @@ import { useTranslation } from 'react-i18next';
 
 
 export default function Logo(){
-        const { t } = useTranslation();
+    const { t } = useTranslation();
     return (
-        <p className="uppercase bg-primary-bg p-2 text-primary font-backwards leading-none text-3xl">{t('title')}</p>
+        <img src="/app/assets/images/Logo.svg" className="w-[6.5625rem]" alt=""/>
     )
 }
