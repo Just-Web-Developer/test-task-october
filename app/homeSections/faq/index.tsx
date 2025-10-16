8c7c59dd3c6e91b4cc3f9eb42e7ff13111e5ee99
Vladyslav Smolianets <vsmolianets@stalker2.com>
2025-10-17 01:44:56 +0300

    FAQ is ready

--- app/homeSections/faq/index.tsx
+++ app/homeSections/faq/index.tsx
@@ -1,15 +1,32 @@
 import {SectionHeader} from "~/components/SectionHeader";
 import {useTranslation} from 'react-i18next';
+import {FAQItem} from "~/homeSections/faq/faqItem";
 
 
 export function FAQ(){
     const {t} = useTranslation();
+    let faqItems = [...t('home.faq.questions', {returnObjects: true})]
+    let lastIndexWithBottom = (faqItems.length - ((faqItems.length % 3) || 3))
+    faqItems = faqItems.map((faqItem, i) => {
+        faqItem.borderStyles = []
+        if (i !== faqItems.length - 1) faqItem.borderStyles.push('border-b-1')
+        if ((i + 1) % 3 !== 0) faqItem.borderStyles.push('border-r-1')
+        if (i >= lastIndexWithBottom) faqItem.borderStyles.push('md:border-b-0')
 
+
+
+        faqItem.borderStyles = faqItem.borderStyles.join(' ')
+        return faqItem
+    })
     return (
         <section>
             <SectionHeader text={t("home.faq.sectionName")}/>
-            <div className="grid grid-cols-3 border-white/20 border-t-1 border-b-1 mb-10 ">
-
+            <div className="grid grid-cols-1 md:grid-cols-3 border-white/20 border-t-1 border-b-1 mb-10 ">
+                {faqItems.map((faqItem, i) => {
+                    return (
+                        <FAQItem key={i} question={faqItem.question} answer={faqItem.answer} borderStyles={faqItem.borderStyles}/>
+                    )
+                })}
             </div>
         </section>
     )
