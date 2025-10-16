56bf2ef0b4a4672a8c86416ff9f714e75cefc148
Vlad Smolianets <itworks.by.vlad@gmail.com>
2025-10-16 16:45:21 +0300

    Removed unneeded text

--- app/assets/locales/index.ts
+++ app/assets/locales/index.ts
@@ -1,7 +1,5 @@
 import * as ua from "~/assets/locales/ua.json"
-import * as en from "~/assets/locales/en.json"
 
 export default {
-    ua: {translation: ua},
-    en: {translation: en}
+    ua: {translation: ua}
 }
